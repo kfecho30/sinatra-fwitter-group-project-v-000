ccc1e652e466a06c2e6df2a6b3b5126cc4cad3f0
Kelsey Fecho <kfecho30@gmail.com>
2018-04-22 17:07:58 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -40,7 +40,14 @@ class ApplicationController < Sinatra::Base
   end
 
   post '/login' do
-    
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/tweets'
+    else
+      redirect '/login'
+    end
   end
 
   get '/tweets' do
