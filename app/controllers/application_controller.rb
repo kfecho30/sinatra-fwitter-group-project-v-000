24e4c716a4cceaef755594455c674c0c8ee835aa
Kelsey Fecho <kfecho30@gmail.com>
2018-04-22 18:03:01 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -120,7 +120,7 @@ class ApplicationController < Sinatra::Base
   post '/tweets/:id/delete' do
     binding.pry
     @tweet = Tweet.find(params[:id])
-    if logged_in? && @tweet.id == session.user_id
+    if logged_in? && @tweet.user_id == session.user_id
       @tweet.delete
       redirect '/tweets'
     else
